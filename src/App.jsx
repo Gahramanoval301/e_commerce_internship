0dd5eac3cb1f7f273075303885a326a0ffbfa0f9
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-01 22:32:34 +0400

    setup of react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import WebRouting from './routing'
+
+function App() {
+
+  return (
+    <>
+      <WebRouting/>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          dark: '#B3C8CF',
+          light:'#97E7E1'
+        },
+        secondary: {
+          light: '#F8F6E3', 
+          dark:'#F1EEDC'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
