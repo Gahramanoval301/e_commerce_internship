5a30d3140b90f5d3f320081211d04e71dec4e293
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-01 22:40:57 +0400

    creting home page file

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageContainer from '../components/PageContainer'
+
+export default function Home() {
+    return (
+      <PageContainer>
+            
+          <div className='font-bold text-emerald-300 h-[200vh]'>Home</div>
+      </PageContainer>
+  )
+}
