46c488f415c73d803f33d598e4bb2f037d9e3514
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-02 16:53:53 +0400

    adding side-bar section

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
+import Category from '../Category'
 
 export default function PageContainer({ children }) {
     return (
         <>
             <Header />
-            <main className='relative top-[110px]'>
-                {children}
+            <main className='relative top-[110px] grid grid-cols-12'>
+                <div className='col-span-2 border-2 border-red-500'>
+                    <Category />
+                </div>
+                <div className='border-2 border-green-500 col-span-10'>
+                    {children}
+                </div>
             </main>
             <Footer />
         </>
