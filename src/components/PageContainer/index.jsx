1b8f023977b8d0c6bb1b2eab88aa56dc89ab4b37
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-04 00:42:14 +0400

    change some designs of main sections

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
 import Category from '../Category'
 
 export default function PageContainer({ children }) {
+
     return (
         <>
             <Header />
             <main className='relative top-[110px] grid grid-cols-12'>
-                <div className='col-span-2 border-2 border-red-500'>
-                    <Category />
+                <div className='col-span-2 border-r-[1px] border-primary-dark'>
+                    <Category  />
                 </div>
-                <div className='border-2 border-green-500 col-span-10'>
+                <div className='col-span-10'>
                     {children}
                 </div>
             </main>
