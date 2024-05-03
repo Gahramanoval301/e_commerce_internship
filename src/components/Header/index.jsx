414c9d195834fbd8d8e1916eb8501c660108accf
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-04 00:41:35 +0400

    create dispatch for searched text and add searched value to redux tk value

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { updateSearchedText } from '../../slices/OptionCategorySlice';
 
 export default function Header() {
+    const [searchedValue, setSearchedValue] = useState('');
+
+    const dispatch = useDispatch();
+
+    const handleChange = (event) => {
+        setSearchedValue(event.target.value)
+        
+        dispatch(updateSearchedText(event.target.value));
+    }
+
     return (
         <header className='w-full fixed bg-secondary-light shadow-md z-50'>
             <div className='flex justify-between items-center p-8'>
                 <h1 className='font-bold text-3xl tracking-wide drop-shadow-lg'>Leman <span className='text-primary-dark'>Shoes</span></h1>
                 <div className='w-3/5 border-2 p-2 border-primary-dark'>
-                    <input className='w-11/12 outline-none bg-transparent tracking-wide' type="text" placeholder='Enter your product name...' />
+                    <input className='w-11/12 outline-none bg-transparent tracking-wide' type="text"
+                        placeholder='Enter your product name...'
+                        value={searchedValue}
+                        onChange={handleChange}
+                    />
                     <button type='submit' className='ml-5'>
                         <i className="fa-solid fa-magnifying-glass"></i>
                     </button>
