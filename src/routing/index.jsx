d105f3bb0a292bc0b9d91afeaa127638e2e6f2f8
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-04 00:44:34 +0400

    add products slug roouter to webrouting

--- src/routing/index.jsx
+++ src/routing/index.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router"
 import Home from "../pages/home"
+import ProductSlug from "../components/ProductSlug"
 
 const WebRouting = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home  />} />
+            <Route path="/:slug" element={<ProductSlug />} />
         </Routes>
     )
 }
