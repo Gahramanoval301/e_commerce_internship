66351a76d6a8123cc26c6f2f61af6cadb6c35211
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-04 00:44:10 +0400

    add products component to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageContainer from '../components/PageContainer'
+import Products from '../components/Products'
 
-export default function Home() {
-    return (
-      <PageContainer>
-            
-          <div className='font-bold text-emerald-300 h-[200vh]'>Home</div>
+export default function Home({}) {
+  return (
+      <PageContainer >
+        <div className='font-bold h-[200vh] '>
+          <Products/>
+          </div>
       </PageContainer>
   )
 }
