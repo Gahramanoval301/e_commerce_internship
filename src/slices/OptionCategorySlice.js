c4c18d58637566ef5171fad324f5b3906b1288e0
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-08 00:13:32 +0400

    adding other filter values of products

--- src/slices/OptionCategorySlice.js
+++ src/slices/OptionCategorySlice.js
@@ -3,7 +3,10 @@ import { createSlice } from "@reduxjs/toolkit"
 const initialState = {
     sex_value: '',
     type_value: '',
-    searched_text: ''
+    searched_text: '',
+    use_value: '',
+    price_value: '',
+    color_value: ''
 }
 const OptionCategorySlice = createSlice({
     name: 'options',
@@ -17,9 +20,18 @@ const OptionCategorySlice = createSlice({
         },
         updateSearchedText: (state, action) => {
             state.searched_text = action.payload;
+        },
+        updateUseSelectedValue: (state, action) => {
+            state.use_value = action.payload;
+        },
+        updatePriceSelectedValue: (state, action) => {
+            state.price_value = action.payload;
+        },
+        updateColorSelectedValue: (state, action) => {
+            state.color_value = action.payload;
         }
     }
 })
 
-export const { updateSexSelectedValue, updateTypeSelectedValue, updateSearchedText } = OptionCategorySlice.actions;
+export const { updateSexSelectedValue, updateTypeSelectedValue, updateSearchedText, updateUseSelectedValue, updatePriceSelectedValue, updateColorSelectedValue } = OptionCategorySlice.actions;
 export default OptionCategorySlice.reducer;
