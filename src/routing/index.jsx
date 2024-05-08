ff14044546aba6ed368d637d52e3191bc7a0b13b
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-09 00:48:48 +0400

    add cart routing

--- src/routing/index.jsx
+++ src/routing/index.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router"
 import Home from "../pages/home"
 import ProductSlug from "../pages/ProductSlug"
+import Cart from "../components/Cart"
 
 const WebRouting = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home  />} />
+            <Route path="/" element={<Home />} />
             <Route path="/:slug" element={<ProductSlug />} />
+            <Route path="/cart" element={<Cart />} />
         </Routes>
     )
 }
