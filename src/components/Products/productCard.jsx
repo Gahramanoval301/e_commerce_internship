c5bc46a5bede027b8f21a26a0a790e5c2b1aa8d5
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-09 00:48:35 +0400

    adding addToCart function to product card

--- src/components/Products/productCard.jsx
+++ src/components/Products/productCard.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useReducer } from 'react'
 import { Link } from "react-router-dom";
+import { initialState, reducer, types } from '../../reducer';
+
 
 export default function ProductCard({ product }) {
+    const url = 'https://65217450a4199548356d3a5c.mockapi.io/api/v1/products'
+    const cart_url = 'https://65217450a4199548356d3a5c.mockapi.io/api/v1/cart'
+
     const renderStars = () => {
         const stars = [];
         for (let i = 0; i < product.star; i++) {
@@ -9,6 +15,36 @@ export default function ProductCard({ product }) {
         }
         return stars;
     }
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    useEffect(() => {
+        axios.get(url).then(({ data }) => {
+            dispatch({ type: types.GET_PRODUCTS, payload: data[0].products });
+        })
+
+
+    }, [])
+    const addToCart = (itemId) => {
+        const addedItem = state.products.find(({ id }) => id === itemId)
+        addedItem.numbersOfProduct += 1
+        console.log(addedItem.isAdded, addedItem.numbersOfProduct);
+
+        if (addedItem.isAdded === "false") {
+            axios.post(cart_url, addedItem)
+                .then(function (response) {
+                    console.log(response);
+                })
+        }
+        axios.put(`${cart_url}/${itemId}`, { ...addedItem, "numbersOfProduct": addedItem.numbersOfProduct })
+            .then((response) => {
+                console.log(response);
+            })
+        console.log(addedItem.isAdded);
+        addedItem.isAdded = "true"
+    }
+    // console.log(state.products);
+
     return (
         <Link to={`/${product.slug}`}>
             <div className='product_card'>
@@ -21,7 +57,7 @@ export default function ProductCard({ product }) {
                     <p>{product.price}</p>
                     <div className='icons_box'>
                         <Link><i className="fa-solid fa-heart hover:rotate-45 hover:text-red-600"></i></Link>
-                        <Link><i className="fa-solid fa-cart-plus hover:text-primary-darkest hover:scale-105"></i></Link>
+                        <Link onClick={() => addToCart(product.id)}><i className="fa-solid fa-cart-plus hover:text-primary-darkest hover:scale-105"></i></Link>
                     </div>
                 </div>
             </div>
