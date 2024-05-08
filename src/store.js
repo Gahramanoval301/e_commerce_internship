d35ed31fdd0ef4b35f2480f32d421020d4978fe1
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-09 00:46:43 +0400

    creating cartItemsReducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import optionReducer from "./slices/OptionCategorySlice";
+import cartItemsReducer from "./slices/CartItemsSlice";
 
 export const store = configureStore({
     reducer: {
         options: optionReducer,
+        cartItems: cartItemsReducer
     }
 })
