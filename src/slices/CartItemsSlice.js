962881421bb33177c3140148f2819bbafc2fc885
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-10 01:25:12 +0400

    add methods of card items to cart_items_slice

--- src/slices/CartItemsSlice.js
+++ src/slices/CartItemsSlice.js
@@ -1,13 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    items: []
+    all_items: [],
+    items: [],
+    item_counts: []
 }
 const CartItemsSlice = createSlice({
     name: 'options',
     initialState,
     reducers: {
+        getAllItems: (state, action) => {
+            state.all_items = action.payload;
+        },
+        addItem: (state, action) => {
+            state.items.push(action.payload);
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter(item => item.id !== action.payload);
+        },
+        setCounts: (state, action) => {
+            state.item_counts = action.payload;
+        }
     }
 })
-
+export const { addItem, setCounts, getAllItems } = CartItemsSlice.actions
 export default CartItemsSlice.reducer;
