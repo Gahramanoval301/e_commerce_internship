ceb789f8535b80c3c980500c0ef1c8810849ae1a
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-12 00:37:55 +0400

    delete dispatch , create totalPrice and totalAmount funcs, update designs

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -5,51 +5,33 @@ import { useNavigate } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function Cart() {
-    const dispatch_amount = useDispatch();
-    // const cart_url = 'https://65217450a4199548356d3a5c.mockapi.io/api/v1/cart';
+
     const navigate = useNavigate();
+
     const [cartItems, setCartItems] = useState([])
 
     const card_items = useSelector((state) => state.cartItems.items)
     const all_items = useSelector((state) => state.cartItems.all_items)
-    console.log(all_items);
 
 
     const counts = {};
+
     card_items.forEach(item => {
         counts[item.id] = (counts[item.id] || 0) + 1;
     });
     console.log(counts);
 
 
-    useEffect(() => {
 
-    }, [])
-
+    //navigate to home page automatically
     const continueToShop = () => {
         navigate('/')
     }
 
-    const totalPrice = card_items.reduce((acc, item) => {
-        return acc + (Number(item.price.slice(1)).toFixed() * item.numbersOfProduct)
-    }, 0)
-
-    const sumObjectValues = (obj) => {
-        let sum = 0;
-        for (const key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                sum += obj[key];
-            }
-        }
-        return sum;
-    }
-
-    const totalSum = sumObjectValues(counts);
-
-
-    const [uniqueItems, setUniqueItems] = useState([]);
 
     // Function to remove duplicates from card_items
+    const [uniqueItems, setUniqueItems] = useState([]);
+
     const removeDuplicates = () => {
         const uniqueItemsMap = new Map();
         card_items.forEach(cardItem => {
@@ -63,41 +45,77 @@ export default function Cart() {
         removeDuplicates();
     }, [card_items]);
 
-    
+    const calculateTotalPrice = (counts) => {
+        let price = 0;
+        for (const key in counts) {
+            if (counts.hasOwnProperty(key)) {
+                const myItem = uniqueItems.find(item => item.id === parseInt(key));
+                if (myItem) {
+                    price += counts[key] * (Number(myItem.price.slice(1))).toFixed(2);
+                }
+            }
+        }
+        return price;
+    }
+    const totalPrice = calculateTotalPrice(counts)
+
+    //total items' amount
+    const sumObjectValues = (obj) => {
+        let sum = 0;
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                sum += obj[key];
+            }
+        }
+        return sum;
+    }
+
+    const totalSum = sumObjectValues(counts);
+
     return (
-            <div className='grid grid-cols-10 gap-5'>
-                <div className='col-span-8'>
-                    <h1>Shopping Bag</h1>
-                    <div className='p-5 m-5 border-2 border-secondary-dark rounded-lg shadow-xl'>
-                        <div className='grid grid-cols-10'>
-                            <h1 className='col-span-4'>Product</h1>
-                            <h1 className='col-span-2'>Price</h1>
-                            <h1 className='col-span-2'>Quantity</h1>
-                            <h1 className='col-span-2'>Total Price</h1>
-                        </div>
-                        {uniqueItems.map((cartItem) => {
-                            return <SingleCardItem key={cartItems.id} item={cartItem} counts={counts} card_items={card_items} />
+        <div className='grid grid-cols-10'>
+            <div className='col-span-8'>
+                <h1 className='m-5 font-bold text-3xl tracking-wider'>Shopping Bag</h1>
+                <div className=' m-5 mr-1 border-2 border-secondary-dark rounded-lg shadow-xl '>
+                    <div className='grid grid-cols-10 place-items-center p-3 bg-primary-light rounded-t-lg shadow-md font-semibold '>
+                        <h1 className='col-span-4 '>Product</h1>
+                        <h1 className='col-span-2 '>Price</h1>
+                        <h1 className='col-span-2 '>Quantity</h1>
+                        <h1 className='col-span-2 '>Total Price</h1>
+                    </div>
+                    <div>
+                        {uniqueItems.map((cartItem, index) => {
+                            return <>
+                                <SingleCardItem key={cartItems.id} item={cartItem} counts={counts} card_items={card_items} />
+                                {index !== uniqueItems.length - 1 && <hr className='border-primary-light' />}
+                            </>
                         })}
                     </div>
                 </div>
-                <div className='col-span-2'>
-                    <h1>Checkout</h1>
+            </div>
+            <div className='col-span-2 m-5 flex flex-col justify-end '>
+                <div className='border-2 rounded-xl border-primary-darkest bg-secondary-light p-3 pb-5 flex flex-col gap-5'>
                     <div>
-                        <div>
-                            <p>Total Items</p>
-                            <span>{totalSum}</span>
-                        </div>
-                        <div>
-                            <p>Total Price</p>
-                            <span>{totalPrice}</span>
-                        </div>
+                        <h1 className='font-bold tracking-wider text-pink-600 mb-1'>Checkout</h1>
+                        <hr className=' border-primary-darkest' />
                     </div>
                     <div>
-                        <button>Apply</button>
-                        <button onClick={continueToShop}>Continue to Shop</button>
+                        <div className='info-section'>
+                            <p>Total Items:</p>
+                            <span>{totalSum}</span>
+                        </div>
+                        <div className='info-section'>
+                            <p>Total Price</p>
+                            <span>${totalPrice}</span>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-2 '>
+                        <button className='btn-product'>Apply</button>
+                        <button className='btn-product' onClick={continueToShop}>Continue to Shop</button>
                     </div>
                 </div>
             </div>
+        </div>
 
-        )
-    }
+    )
+}
