a0dacda4db799e6fc8491587763675ed13d7cb80
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-12 00:39:06 +0400

    create navigate functionality and add calculation totalPrice of single cart items and finally update designs

--- src/components/Cart/SingleCardItem.jsx
+++ src/components/Cart/SingleCardItem.jsx
@@ -1,27 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 export default function SingleCardItem({ item, counts, card_items }) {
- 
-    console.log(item, counts);
 
+    const navigate = useNavigate();
 
     //calculateTotalPrice
     const calculateTotalPrice = () => {
-        return Number(item.price.slice(1)).toFixed() * item.numbersOfProduct;
+        return (Number(item.price.slice(1)) * counts[item.id]).toFixed(2);
     }
 
-    const isMultiple = counts[item.id] > 1;
+    //navigate to product slug
+    const navigateToProductSlug = () => {
+        navigate(`/product/${item.slug}`);
+    }
 
     return (
-        <div className='grid grid-cols-10'>
-            <div className='col-span-4 flex gap-2'>
-                <div>
+        <div className='grid grid-cols-10 place-items-center mb-5 p-4' onClick={navigateToProductSlug}>
+            <div className='col-span-4 flex gap-2 mt-5 '>
+                <div className='w-44 h-44 flex justify-start items-center'>
                     <img src={item.images.normal} alt={item.title} />
                 </div>
-                <div className='flex flex-col justify-between'>
+                <div className='flex flex-col justify-between ml-3 py-3'>
                     <div>
                         <span className='text-xs text-gray-400 uppercase font-semibold'>{item.sex}</span>
-                        <h3 className='tracking-wide font-bold text-md'>{item.title}</h3>
+                        <h3 className='tracking-wide font-bold text-lg capitalize'>{item.title}</h3>
                     </div>
                     <div>
                         <div className='info-section'>
@@ -33,12 +35,7 @@ export default function SingleCardItem({ item, counts, card_items }) {
                             <span>{item.material}</span>
                         </div>
                         {/* Render count only if it's greater than 1 */}
-                        {isMultiple && (
-                            <div className='info-section'>
-                                <p>Count:</p>
-                                <span>{counts[item.id]}</span>
-                            </div>
-                        )}
+
                     </div>
                 </div>
             </div>
@@ -46,7 +43,7 @@ export default function SingleCardItem({ item, counts, card_items }) {
                 <p>{item.price}</p>
             </div>
             <div className='col-span-2'>
-                <p>{item.numbersOfProduct}</p>
+                <p>{counts[item.id]}</p>
             </div>
             <div className='col-span-2'>
                 ${calculateTotalPrice()}
