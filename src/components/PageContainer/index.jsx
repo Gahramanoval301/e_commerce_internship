f7548ef1e7fd48e7d96e40cc9b1124994a2530cc
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-17 22:09:01 +0400

    add some styles for receive responsive style

--- src/components/PageContainer/index.jsx
+++ src/components/PageContainer/index.jsx
@@ -8,10 +8,12 @@ export default function PageContainer({ children }) {
         <>
             <Header />
             <main className='relative top-[110px] grid grid-cols-12'>
-                <div className='col-span-2 border-r-[1px] border-primary-dark'>
-                    <Category  />
-                </div>
-                <div className='col-span-10'>
+                {
+                    <div className='hidden md:block col-span-3 lg:col-span-2 border-r-[1px] border-primary-dark'>
+                        <Category />
+                    </div>
+                }
+                <div className='col-span-9 lg:col-span-10'>
                     {children}
                 </div>
             </main>
