d216fa78ed0d90a461c3c8448d4156cfd6cfa3a9
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-19 23:10:38 +0400

    add some styles for responsive design

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -4,6 +4,8 @@ import SingleCardItem from './SingleCardItem';
 import { useNavigate } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCounts } from '../../slices/CartItemsSlice';
+import Header from '../Header';
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
 
@@ -87,49 +89,52 @@ export default function Cart() {
     const totalSum = sumObjectValues(countsSlice);
 
     return (
-        <div className='grid grid-cols-10'>
-            <div className='col-span-8'>
-                <h1 className='m-5 font-bold text-3xl tracking-wider'>Shopping Bag</h1>
-                <div className=' m-5 mr-1 border-2 border-secondary-dark rounded-lg shadow-xl '>
-                    <div className='grid grid-cols-10 place-items-center p-3 bg-primary-light rounded-t-lg shadow-md font-semibold '>
-                        <h1 className='col-span-4 '>Product</h1>
-                        <h1 className='col-span-2 '>Price</h1>
-                        <h1 className='col-span-2 '>Quantity</h1>
-                        <h1 className='col-span-2 '>Total Price</h1>
+        <>
+            <Header />
+            <div className='grid lg:grid-cols-10 translate-y-44 sm:translate-y-32 md:translate-y-28 pb-10'>
+                <div className='col-span-8 pr-5'>
+                    <h1 className='m-5 font-bold text-3xl tracking-wider'>Shopping Bag</h1>
+                    <div className='m-5 mr-1 border-2 border-secondary-dark rounded-lg shadow-xl '>
+                        <div className='hidden sm:grid sm:grid-cols-10 place-items-center p-3 bg-primary-light rounded-t-lg shadow-md font-semibold '>
+                            <h1 className='col-span-4 '>Product</h1>
+                            <h1 className='col-span-2 '>Price</h1>
+                            <h1 className='col-span-2 '>Quantity</h1>
+                            <h1 className='col-span-2 '>Total Price</h1>
+                        </div>
+                        <div>
+                            {uniqueItems.map((cartItem, index) => {
+                                return <>
+                                    <SingleCardItem key={cartItems.id} item={cartItem} counts={countsSlice} card_items={cartItems} />
+                                    {index !== uniqueItems.length - 1 && <hr className='border-primary-light' />}
+                                </>
+                            })}
+                        </div>
                     </div>
-                    <div>
-                        {uniqueItems.map((cartItem, index) => {
-                            return <>
-                                <SingleCardItem key={cartItems.id} item={cartItem} counts={countsSlice} card_items={cartItems} />
-                                {index !== uniqueItems.length - 1 && <hr className='border-primary-light' />}
-                            </>
-                        })}
+                </div>
+                <div className='text-center max-[1024px]:w-[90vw] max-[600px]:w-[290px] max-[600px]:ml-5 max-[850px]:ml-6 max-[1024px]:ml-10 col-span-2 m-5 flex flex-col justify-end '>
+                    <div className='border-2 rounded-xl border-primary-darkest bg-secondary-light p-3 pb-5 flex flex-col gap-5'>
+                        <div>
+                            <h1 className='font-bold tracking-wider text-pink-600 mb-1'>Checkout</h1>
+                            <hr className=' border-primary-darkest' />
+                        </div>
+                        <div>
+                            <div className='info-section'>
+                                <p>Total Items:</p>
+                                <span>{totalSum}</span>
+                            </div>
+                            <div className='info-section'>
+                                <p>Total Price</p>
+                                <span>${totalPrice}</span>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-2 '>
+                            <button className='btn-product'>Apply</button>
+                            <button className='btn-product' onClick={continueToShop}>Continue to Shop</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className='col-span-2 m-5 flex flex-col justify-end '>
-                <div className='border-2 rounded-xl border-primary-darkest bg-secondary-light p-3 pb-5 flex flex-col gap-5'>
-                    <div>
-                        <h1 className='font-bold tracking-wider text-pink-600 mb-1'>Checkout</h1>
-                        <hr className=' border-primary-darkest' />
-                    </div>
-                    <div>
-                        <div className='info-section'>
-                            <p>Total Items:</p>
-                            <span>{totalSum}</span>
-                        </div>
-                        <div className='info-section'>
-                            <p>Total Price</p>
-                            <span>${totalPrice}</span>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-2 '>
-                        <button className='btn-product'>Apply</button>
-                        <button className='btn-product' onClick={continueToShop}>Continue to Shop</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </>
 
     )
 }
