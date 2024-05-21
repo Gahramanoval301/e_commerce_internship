1d2018e2fa8882fd350285f86358e1eec8e2e10d
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-22 01:16:42 +0400

    adding favourites reducer to the store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import optionReducer from "./slices/OptionCategorySlice";
 import cartItemsReducer from "./slices/CartItemsSlice";
+import FavouritesReducer from "./slices/FavouriteSlice";
 
 export const store = configureStore({
     reducer: {
         options: optionReducer,
-        cartItems: cartItemsReducer
+        cartItems: cartItemsReducer,
+        favourites: FavouritesReducer
     }
 })
