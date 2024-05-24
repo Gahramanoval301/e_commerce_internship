92c78bb2952c96e57b8c3ffb244413f6043f8454
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-24 22:31:14 +0400

    adding filter functionality to the favourites section of e-commerce web page

--- src/components/Favourites/index.jsx
+++ src/components/Favourites/index.jsx
@@ -2,11 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Header from '../Header';
 import FavouriteCard from './FavouriteCard';
+import Loading from '../Loading';
+import { Link } from 'react-router-dom';
 
 const Favourites = () => {
   const favourited_items = useSelector((state) => state.favourites.favourited_items)
   const [uniqueStoredFavourites, setUniqueStoredFavourites] = useState([])
   const [storedFavourites, setStoredFavourites] = useState([])
+  const [isDeletedFromFavourite, setIsDeletedFromFavourite] = useState(false);
+  const searchText = useSelector((state) => state.options.searched_text);
+  const selectedSexValue = useSelector((state) => state.options.sex_value);
+  const selectedTypeValue = useSelector((state) => state.options.type_value);
+  const selectedUseValue = useSelector((state) => state.options.use_value);
+  const selectedPriceValue = useSelector((state) => state.options.price_value);
+  const selectedColorValue = useSelector((state) => state.options.color_value);
+  const [isLoading, setIsLoading] = useState(true);
+
 
   console.log(favourited_items);
 
@@ -14,6 +25,8 @@ const Favourites = () => {
     //fetch stored items from local storage
     const storedFavouritesLocal = JSON.parse(localStorage.getItem('favourites')) || [];
     setStoredFavourites(storedFavouritesLocal)
+    console.log(searchText);
+
   }, [])
 
 
@@ -24,6 +37,8 @@ const Favourites = () => {
       uniqueStoredFavouritesMap.set(storedFavourite.id, storedFavourite);
     });
     setUniqueStoredFavourites([...uniqueStoredFavouritesMap.values()]);
+    setIsLoading(false);
+
   }
 
   //when stored favorites are updated then again check for duplicates
@@ -31,21 +46,81 @@ const Favourites = () => {
     removeDuplicates()
   }, [storedFavourites])
 
+  //reload Page
+  const ReloadPage = () => {
+    location.reload();
+  }
+  //return products which specify specific filters
+  const filteredProducts = uniqueStoredFavourites.filter(
+    ({ sex, category, title, use, color, priceDegree }) => {
+      const sexMatch = !selectedSexValue || sex === selectedSexValue;
+      const typeMatch =
+        selectedTypeValue === "all_category" || category === selectedTypeValue;
+      const titleMatch =
+        !searchText || title.toLowerCase().includes(searchText.toLowerCase());
+      const useMatch =
+        selectedUseValue === "all_use" || use === selectedUseValue;
+      const colorMatch =
+        selectedColorValue === "all_color" || color === selectedColorValue;
+      const priceMatch =
+        selectedPriceValue === "all_price" ||
+        priceDegree === selectedPriceValue;
 
+      if (
+        (selectedSexValue === "" || sexMatch) &&
+        (selectedTypeValue === "" || typeMatch) &&
+        (searchText === "" || titleMatch) &&
+        (selectedUseValue === "" || useMatch) &&
+        (selectedColorValue === "" || colorMatch) &&
+        (selectedPriceValue === "" || priceMatch)
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  );
 
+  const myProducts =
+    selectedSexValue === "" &&
+      (selectedTypeValue === "all_category" || selectedTypeValue === "") &&
+      searchText === "" &&
+      (selectedUseValue === "" || selectedUseValue === "all_use") &&
+      (selectedColorValue === "" || selectedColorValue === "all_colors") &&
+      (selectedPriceValue === "" || selectedPriceValue === "all_price")
+      ? uniqueStoredFavourites
+      : filteredProducts;
+  
   return (
     <>
-      <Header />
-      <div className='p-8 translate-y-28'>
-        <h1 className='tracking-widest font-bold text-2xl'>FAVOURITES</h1>
-        <div className='grid grid-cols-4 gap-3 mt-5'>
-          {
-            uniqueStoredFavourites.map((item) => {
-              return <FavouriteCard key={item.id} item={item} uniqueFavourites={uniqueStoredFavourites} />
-            })
-          }
-        </div>
+      <div className='fixed md:bottom-5 md:right-5 z-50 bg-red-500 p-2 text-blue-200 font-semibold hover:bg-red-600'>
+        <Link to='/'>Continue to shopping</Link>
       </div>
+      <Header />
+      {isLoading ? (<Loading />) :
+        (
+          <div className='p-5 lg:p-8 translate-y-44 md:translate-y-36 lg:translate-y-28'>
+            <div
+              className={`state-delete  ${isDeletedFromFavourite ? "active" : ""}`}
+              onClick={ReloadPage}
+            >Restart Page</div>
+            <div className='box-navigate  '>
+              <h1 className=''>FAVOURITES</h1>
+              <Link to='/' className=''>Back to Home</Link>
+            </div>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-5 max-[768px]:mr-20'>
+              {
+                myProducts.map((item) => {
+                  return <FavouriteCard key={item.id} item={item} uniqueFavourites={uniqueStoredFavourites}
+                    setIsDeletedFromFavourite={setIsDeletedFromFavourite}
+                  />
+                })
+              }
+            </div>
+          </div>
+        )
+      }
+
     </>
   )
 }
