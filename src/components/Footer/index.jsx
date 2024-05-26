1beadf56aa43497f7d527247ae6c7994c97fc015
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-26 16:36:33 +0400

    create and design Footer section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React from "react";
+import { FaLocationDot } from "react-icons/fa6";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer-box">
+      <a
+        className="font-bold text-3xl tracking-wide drop-shadow-lg cursor-pointer relative z-50"
+        href="/"
+      >
+        Leman{" "}
+        <span span className="text-primary-darkest">
+          Shoes
+        </span>
+      </a>
+      <div className="grid grid-cols-2 gap-8">
+        <div className="footer-pv-info-box">
+          <Link to="https://maps.app.goo.gl/MG3hHjz6H9C7xDG8A" target="_blank">
+            <div className="footer-icon-1">
+              <FaLocationDot className="footer-icon" />
+            </div>
+            <p>Azerbaijan, Baku</p>
+          </Link>
+          <Link to="tel:+994513106096" target='_blank'>
+            <div className="footer-icon-1">
+              <BsFillTelephoneFill className="footer-icon" />
+            </div>
+            <p>+994 51 310 60 96</p>
+          </Link>
+          <Link to="mailto:gahramanovalamann@gmail.com" target='_blank'>
+            <div className="footer-icon-1">
+              <SiGmail className="footer-icon" />
+            </div>
+            <p>gahramanovalamann@gmail.com</p>
+          </Link>
+        </div>
+        <div className="footer-about">
+          <h3 className="font-bold">About Web Page</h3>
+          <p className="text-gray-800 text-sm">
+            On that web page, I used third-party packages such as SweetAlerts,
+            React Icons, React Redux, and React Router. For designing, I
+            utilized Tailwind CSS. The website encompasses full functionality except signing because of backend process,
+            and I utilize localhost as a mini-database. This is my mini frontend
+            e-commerce shoe website.{" "}
+          </p>
+          <div className="flex gap-3">
+            <a href="https://github.com/Gahramanoval301" target='_blank' className="relative z-50">
+              <div className="p-2 bg-pink-200">
+                <FaGithub className="footer-icon" />
+              </div>
+            </a>
+            <a href="https://www.linkedin.com/in/leman-gahramanova-0a3b6b2a5" target='_blank'>
+              <div className="footer-icon-2">
+                <FaLinkedin className="footer-icon" />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
